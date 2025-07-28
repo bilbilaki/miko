6d6a5cca3a8c1a0369d5ed52c038fae6060d2dca
Inosuke Hashibira <99578162+bilbilaki@users.noreply.github.com>
2025-07-28 15:21:27 +0530

    Update consts.dart

--- lib/aiconfig/consts.dart
+++ lib/aiconfig/consts.dart
@@ -1,3 +1,3 @@
 const String kApiBaseUrl = "https://api.example.com";
-const String kApiKey = "AIzaSyCvT2Pj99Tp46UMmGSM_H9sS-1tETH2XHs";
+const String kApiKey = "";
 const String kUserAgent = "YourAppName/1.0";
